cd4b187d78d5a8cade266901bae8257c124eea42
Jairo Diaz <jdiaz@singularagency.co>
2025-02-13 18:33:27 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# example-agora
+ 
---
+++ lib/examples/basic/index.dart
@@ -0,0 +1,15 @@
+import 'package:agora_rtc_engine_example/examples/basic/join_channel_video/join_channel_video.dart';
+
+/// Data source for basic examples
+final basic = [
+  //{'name': 'Basic'},
+  // {'name': 'JoinChannelAudio', 'widget': const JoinChannelAudio()},
+  {'name': 'JoinChannelVideo', 'widget': const JoinChannelVideo()},
+  // if (defaultTargetPlatform == TargetPlatform.android &&
+  //     ExampleConfigOverride().isInternalTesting)
+  //   {
+  //     'name': 'FlutterTextureAndroidTest',
+  //     'widget': const FlutterTextureAndroidTest()
+  //   },
+  // {'name': 'StringUid', 'widget': const StringUid()}
+];
